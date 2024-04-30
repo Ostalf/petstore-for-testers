c5fb1043c816510e99e9e0d46f72b5f09a1db9d5
Ostalf <ostalfgladyre@gmail.com>
2024-04-30 13:14:06 +0700

    init

---
+++ src/main/java/com/ostalf/petstore_for_testers/PetstoreForTestersApplication.java
@@ -0,0 +1,13 @@
+package com.ostalf.petstore_for_testers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstoreForTestersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetstoreForTestersApplication.class, args);
+	}
+
+}
